727bb123f49ac6d5dfe445eb2be87138d0ad7bd3
Alexey Solovyev <solovaa@yandex.ru>
2025-04-24 22:15:40 +0000

    add PATH_EMPLOYERS

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 PATH_ROOT = os.path.dirname(__file__)
 PATH_DATA = os.path.join(PATH_ROOT, "data")
 PATH_AREAS = os.path.join(PATH_DATA, "areas.json")
+PATH_EMPLOYERS = os.path.join(PATH_DATA, "watch_list_of_employers.json")
