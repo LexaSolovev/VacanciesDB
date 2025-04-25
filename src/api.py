cdcd82d5dc76ed72e5b6d51b253e0b128feb4023
Alexey Solovyev <solovaa@yandex.ru>
2025-04-25 21:55:01 +0000

    fix api

--- src/api.py
+++ src/api.py
@@ -14,9 +14,9 @@ def get_employer_data(emp_hh_id: int) -> dict:
     if response.status_code == 200:
         content = json.loads(response.content)
         return {
+            "employer_id": emp_hh_id,
             "name": content["name"],
             "url": content["alternate_url"],
-            "hh_id": emp_hh_id,
             "area_id": content["area"]["id"]
         }
     else:
@@ -27,7 +27,7 @@ def get_vacancies_by_employer(emp_hh_id: int) -> list[dict]:
     """Функция получает список вакансий по id работодателя с сайта hh.ru"""
     response = requests.get(
         "https://api.hh.ru/vacancies",
-        params={'emploer_id': emp_hh_id},
+        params={'employer_id': emp_hh_id, 'per_page': 100},
         headers={'User-Agent': 'HH-User-Agent'}
     )
     if response.status_code == 200:
@@ -38,6 +38,7 @@ def get_vacancies_by_employer(emp_hh_id: int) -> list[dict]:
         requests.RequestException(f"Неверный ответ! Статус-код = {response.status_code}")
 
 if __name__ == "__main__":
+    pass
     # with open(PATH_EMPLOYERS) as f:
     #     emp_list = json.load(f)["emp_hh_id"]
     # for emp_id in emp_list:
